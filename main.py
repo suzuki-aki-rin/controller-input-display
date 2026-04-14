85879b282a0c7e25a14bd99da88625ef36b21535
suzuki-aki-rin <aki.suzuki.rin@gmail.com>
2026-04-14 09:12:25 +0900

    fix: modify error handling for ctrl-C
    There are KeyboardInterrupt, asyncio.CanceledError and ExceptionGroup.
    should pass through ExceptionGroup?

--- main.py
+++ main.py
@@ -13,7 +13,8 @@ from logger import InputLogger
 import logging
 
 logging.basicConfig(
-    level=logging.INFO,
+    # level=logging.INFO,
+    level=logging.DEBUG,
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
 )
 logger = logging.getLogger(__name__)
@@ -91,7 +92,16 @@ async def run(args):
             tg.create_task(event_reader(device, state))
             # poll_loop read controller state per frame (1/60sec)
             tg.create_task(poll_loop(state, history, log))
+    except* asyncio.CancelledError:
+        # except* expands ExceptionGroup
+        # ExceptionGroup happens, then all group tasks are cancelled
+        # this raise asyncio.exceptions.CancelledError causes KeyboardInterrupt that asyncio.run() raises.
+        # without this except*, it also causes KeyboardInterrupt that asyncio.run() raises.
+
+        # logger.debug("task group(event_reader, poll_loop) is cancelled")
+        raise asyncio.CancelledError
     finally:
+        logger.debug("task group(event_reader, poll_loop) is cancelled or finished")
         if log:
             log.close()
 
@@ -121,96 +131,8 @@ def main():
     try:
         asyncio.run(run(args))
     except KeyboardInterrupt:
-        logger.info("app is stopped.")
+        logger.info("app is stopped by KeyboardInterrupt")
 
 
 if __name__ == "__main__":
     main()
-
-#
-# def main():
-#     # ------- ArgumentParser
-#
-#
-#     # -------- Device detection
-#
-#     device = find_device(args.device_name)
-#     if not device:
-#         if args.device_name:
-#             logger.error("argument device: %s not found.", args.device_name)
-#         else:
-#             logger.error("default device: %s not found.", DEFAULT_DEVICE_NAME)
-#         return
-#
-#     logger.info("Logging: %s", device.name)
-#     if args.logfile:
-#         logger.info("Log file: %s", args.logfile)
-#     logger.info("Format:  frames : direction  buttons")
-#
-#     #  ========== Device polling ========================================
-#
-#     # state is used to get controller state in the thread
-#     state = ControllerState()
-#     # Use to stop device.read_loop() in the thread
-#     stop_event = threading.Event()
-#
-#     # Create thread to read device event
-#     t = threading.Thread(
-#         target=event_reader, args=(device, state, stop_event), daemon=True
-#     )
-#     t.start()
-#
-#     # Variables to save the controller state
-#     prev_dirs: set[str] = set()
-#     prev_btns: set[str] = set()
-#
-#     # to store holding frame
-#     hold = 0
-#     # For 1f(1/60 sec) tick
-#     next_tick = time.perf_counter()
-#
-#     # to store input log
-#     history: deque[str] = deque(maxlen=HISTORY_SIZE)
-#     # file logger
-#     log = InputLogger(args.logfile) if args.logfile else None
-#
-#     reserve_display()
-#
-#     # Read device state every frame(1/60 sec)
-#     try:
-#         while True:
-#             next_tick += FRAME_SEC
-#             sleep = next_tick - time.perf_counter()
-#             if sleep > 0:
-#                 time.sleep(sleep)
-#
-#             cur_dirs, cur_btns = state.snapshot()
-#             hold += 1
-#
-#             if cur_dirs != prev_dirs or cur_btns != prev_btns:
-#                 final_line = format_line(hold - 1, prev_dirs, prev_btns)
-#                 if hold - 1 > 0:
-#                     history.appendleft(final_line)
-#                     if log:
-#                         log.write(final_line)
-#
-#                 hold = 1
-#                 prev_dirs = cur_dirs
-#                 prev_btns = cur_btns
-#
-#             live_line = format_line(hold, cur_dirs, cur_btns)
-#             redraw(live_line, history)
-#
-#     except KeyboardInterrupt:
-#         logger.info("\nKeyboard interruption. Stopped.")
-#     finally:
-#         stop_event.set()
-#         device.close()
-#         logger.info("device is closed")
-#         t.join()
-#         if log:
-#             log.close()
-#
-#
-# if __name__ == "__main__":
-#     main()

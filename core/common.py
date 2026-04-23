ccfde58211354fc278a2114347d6e5da41429123
suzuki-aki-rin <aki.suzuki.rin@gmail.com>
2026-04-23 22:57:44 +0900

    refactor: delete unused. rename for better readablity

--- core/common.py
+++ core/common.py
@@ -1,17 +1,32 @@
 from core.constants import NUMPAD, ARROW
 from core.pollers import GamepadHoldedButtons
+from dataclasses import dataclass
 
 
-def format_payload(pressed_buttons: GamepadHoldedButtons) -> dict:
-    numpad = NUMPAD.get(frozenset(pressed_buttons.dirs), "5")
+@dataclass
+class FormattedHoldedButtons:
+    hold_frame: str
+    arrow: str
+    btns: str
+
+
+def format_holded_buttons(holded_btns: GamepadHoldedButtons) -> FormattedHoldedButtons:
+    numpad = NUMPAD.get(frozenset(holded_btns.dirs), "5")
+    sorted_btns = sorted(holded_btns.btns)
+
+    return FormattedHoldedButtons(
+        hold_frame=str(holded_btns.hold_frame),
+        arrow=ARROW[numpad],
+        btns=" ".join(sorted_btns),
+    )
+
+
+def holded_buttons_to_str(holded_btns: GamepadHoldedButtons) -> str:
+    numpad = NUMPAD.get(frozenset(holded_btns.dirs), "5")
+    sorted_btns = sorted(holded_btns.btns)
+
+    hold_frame = str(holded_btns.hold_frame)
     arrow = ARROW[numpad]
-    return {
-        "type": "update",
-        "hold": pressed_buttons.hold_frame,
-        "arrow": arrow,
-        "btns": sorted(pressed_buttons.btns),
-    }
+    btns = " ".join(sorted_btns)
 
-
-def format_payload_to_str(fp: dict) -> str:
-    return f"{fp['hold']} {fp['arrow']} {' '.join(fp['btns'])}"
+    return " ".join((hold_frame, arrow, btns))
